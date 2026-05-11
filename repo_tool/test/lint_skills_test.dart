19228e842c691a5bcdb06b6603f3651953b2af2a
Reid Baker <1063596+reidbaker@users.noreply.github.com>
2026-05-11 12:28:04 -0400

    Add skills linting to repo.  (#22)
    * Adds a package for running tooling on dart-skills and fixes trailing whitespace on existing skills, adds a workflow to ensure that dart code passes analyze and format
    
    * Fix generic package items and remove lockfile
    
    * Add licence header and ignore pubspec

---
+++ repo_tool/test/lint_skills_test.dart
@@ -0,0 +1,36 @@
+// Copyright (c) 2025, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:async';
+import 'package:dart_skills_lint/dart_skills_lint.dart';
+import 'package:logging/logging.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Run skills linter', () async {
+    // Enable logging to see detailed validation errors in test output.
+    final Level oldLevel = Logger.root.level;
+    Logger.root.level = Level.ALL;
+    final StreamSubscription<LogRecord> subscription = Logger.root.onRecord
+        .listen((record) => print(record.message));
+
+    try {
+      final isValid = await validateSkills(
+        skillDirPaths: ['../skills'],
+        resolvedRules: {
+          'check-relative-paths': AnalysisSeverity.error,
+          'check-trailing-whitespace': AnalysisSeverity.error,
+        },
+      );
+      expect(
+        isValid,
+        isTrue,
+        reason: 'Skills validation failed. See above for details.',
+      );
+    } finally {
+      Logger.root.level = oldLevel;
+      await subscription.cancel();
+    }
+  });
+}
